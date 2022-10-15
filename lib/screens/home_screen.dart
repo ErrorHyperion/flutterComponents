9b01e631acd5f56de7b2242ce8ebde859399a7e4
ErrorHyperion <juan.villanueva@eam.edu.co>
2022-10-15 13:57:03 -0500

    feat(): First Commit with Listview and navigation

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_components/models/menu_option.dart';
+import 'package:flutter_components/router/app_routes.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<MenuOption> menuOptions = AppRoutes.menuOptions;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Componentes en Flutter'),
+        ),
+        elevation: 0,
+      ),
+      body: ListView.separated(
+        itemBuilder: (context, i) => ListTile(
+          leading: Icon(menuOptions[i].icon),
+          title: Text(menuOptions[i].name),
+          onTap: () {
+            /* final route = MaterialPageRoute(
+              builder: (context) => const Listview1Screen(),
+            );
+            Navigator.push(context, route); */
+            Navigator.pushNamed(context, menuOptions[i].route);
+          },
+        ),
+        separatorBuilder: (context, index) => const Divider(),
+        itemCount: AppRoutes.menuOptions.length,
+      ),
+    );
+  }
+}
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash',
+    'Final Fantasy'
+  ];
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview Tipo 2'),
+        elevation: 0,
+        backgroundColor: Colors.indigo,
+      ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          title: Text(options[index]),
+          trailing: const Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.indigo,
+          ),
+          onTap: () {
+            final game = options[index];
+            print(game);
+          },
+        ),
+        separatorBuilder: (context, index) => const Divider(),
+        itemCount: options.length,
+      ),
+    );
+  }
+}
